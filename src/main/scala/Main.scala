6f2d8c6d73b5679f8bed04be5b206fb3b5bd350d
Antoine Comte <antoine.comte@gmail.com>
2014-11-27 11:44:05 +0100

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,8 @@
-import _root_.MusicBrainzSupport.{ReleaseGroupId, ReleaseId}
-import _root_.RunActor.Requests
-import akka.actor.{ActorRef, Actor, ActorSystem}
-import akka.dispatch.Futures
+import MusicBrainzSupport.{ReleaseGroupId, ReleaseId}
+import RunActor.{RequestToken, Requests, Response, Results}
+import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
 import spray.client.pipelining._
 import spray.http.HttpHeaders._
 import spray.http.MediaTypes._
@@ -12,6 +13,8 @@ import spray.json.JsObject
 import spray.json.lenses.JsonLenses._
 
 import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
 
 
 object MusicBrainzSupport {
@@ -65,7 +68,7 @@ class AllMusicSupport(implicit val system: ActorSystem) {
 
     releaseGroupIdOfRelease(id).flatMap {
       case Some(i) => fetchReleaseGroup(i).map(x => x.extract[String](allMusicUrl).headOption)
-      case _       => Future.successful(None)
+      case _ => Future.successful(None)
     }
   }
 
@@ -79,28 +82,87 @@ object RunActor {
 
   import MusicBrainzSupport.ReleaseId
 
-  case object startNewRequest
+  case object RequestToken
 
   case class Requests(l: Seq[ReleaseId])
+
+  case class Response(id: ReleaseId, genre: Option[String])
 
   case class Results(r: Map[ReleaseId, String])
 
 }
 
-trait RunActor extends Actor {
+class RunActor extends Actor {
 
   import context.become
 
-  def receive = makeReceive(List.empty, Map.empty, self)
+  val support: AllMusicSupport = new AllMusicSupport()(context.system)
 
-  def makeReceive(requests: Seq[ReleaseId], results: Map[ReleaseId, String], p: ActorRef): Actor.Receive = {
-    case Requests(l) => become(makeReceive(requests.toList ::: l.toList, results, sender()))
-    case startNewRequest => if ()
+  import support.{allMusicGenre, allMusicUrlOfRelease}
+
+  def receive = makeReceive(List.empty, Map.empty, 0, self)
+
+  implicit val disp = context.system.dispatcher
+
+  def runRequest(id: ReleaseId): Unit = allMusicUrlOfRelease(id).flatMap {
+    case Some(url) => allMusicGenre(url)
+    case None => Future.successful(None)
+  }.onComplete {
+    case Success(v) => self ! Response(id, v)
+    case Failure(t) => self ! Response(id, None)
+
+  }
+
+  def makeReceive(requests: Seq[ReleaseId], results: Map[ReleaseId, String], runningRequests: Int, p: ActorRef): Actor.Receive = {
+    case Requests(l) => become(makeReceive(requests.toList ::: l.toList, results, 0, sender()))
+    case RequestToken => requests match {
+      case h :: t => runRequest(h)
+        become(makeReceive(t, results, runningRequests + 1, p))
+      case _ =>
+    }
+    case Response(id, genre) => {
+      val res = genre match {
+        case Some(g) => results + (id -> g)
+        case _ => results
+      }
+      become(makeReceive(requests, res, runningRequests - 1, p))
+      if (requests.size > 0)
+        self ! RequestToken
+      else
+      if (runningRequests == 1) {
+        p ! Results(res)
+        p ! PoisonPill
+      }
+    }
   }
 
 
 }
 
+object MainActor {
+  def props(ids: Seq[ReleaseId]) = Props(new MainActor(ids))
+}
+
+class MainActor(val ids: Seq[ReleaseId]) extends Actor {
+  val runner = context.actorOf(Props[RunActor])
+
+  runner ! Requests(ids)
+  for (i <- 1 to 5) {
+    runner ! RequestToken
+  }
+  var results = Map[ReleaseId, String]()
+
+  def receive = awaitAsk
+
+  def awaitAsk: Actor.Receive = {
+    case _ => context.become(awaitResult(sender()))
+  }
+
+  def awaitResult(a: ActorRef): Actor.Receive = {
+    case Results(r) => a ! r
+  }
+
+}
 
 /**
   */
@@ -108,18 +170,18 @@ object Main extends App {
 
 
   val system = ActorSystem()
-  val support: AllMusicSupport = new AllMusicSupport()(system)
-
-  import support._
-  import support.mbSupport._
 
 
   val id1: ReleaseId = ReleaseId("5000a285-b67e-4cfc-b54b-2b98f1810d2e")
 
-  val res2: Future[Option[String]] = allMusicUrlOfRelease(id1).flatMap {
-    case Some(url) => allMusicGenre(url)
-    case None      => Future.successful(None)
-  }
+  private val mainActorRef: ActorRef = system.actorOf(MainActor.props(List(id1)))
+
+
+  import scala.concurrent.duration._
+  implicit val timeout = Timeout (2.minutes)
+  implicit val dispatcher = system.dispatcher
+  val res2 = (mainActorRef ? "go").mapTo[Map[ReleaseId, String]]
+
 
   // private val res: Future[Option[ReleaseGroupId]] = releaseGroupIdOfRelease(ReleaseId("5000a285-b67e-4cfc-b54b-2b98f1810d2e"))
   //  res.onSuccess {
@@ -135,8 +197,9 @@ object Main extends App {
    }*/
 
   res2.onSuccess {
-    case Some(v) =>
-      println(s"allmusic genre: $v")
+    case x: Map[ReleaseId, String] => for ((k, v) <- x) {
+      println(s"allmusic genre for $k : $v")
+    }
       system.shutdown()
   }
 
